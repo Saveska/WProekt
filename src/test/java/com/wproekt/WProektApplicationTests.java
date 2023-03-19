bfb495b02deef96cfcfefc65f2cc16f18d15c620
Maria <saveska.maria@gmail.com>
2023-03-20 00:40:15 +0100

    initial commit

---
+++ src/test/java/com/wproekt/WProektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wproekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WProektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
