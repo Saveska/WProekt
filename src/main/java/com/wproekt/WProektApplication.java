80e8629ec33348ae8a87237bfab2358407fc3612
Maria <saveska.maria@gmail.com>
2023-03-20 00:40:15 +0100

    initial commit

---
+++ src/main/java/com/wproekt/WProektApplication.java
@@ -0,0 +1,13 @@
+package com.wproekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WProektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WProektApplication.class, args);
+    }
+
+}
