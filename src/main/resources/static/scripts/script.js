742ccb17b6c80a2e071c1bbc03022d6d03489de0
David <arsovskidavid4@gmail.com>
2023-05-12 19:19:59 +0200

    dodavanje na karticki

--- src/main/resources/static/scripts/script.js
+++ src/main/resources/static/scripts/script.js
@@ -1,3 +1,5 @@
+let titleInputBox = $('#titleNoteInput');
+let textNoteInputBox = $('#noteTextInput');
 
 function auto_grow(element) {
     element.style.height = "auto";
@@ -6,21 +8,47 @@ function auto_grow(element) {
 
 
 // Add task to the form when the user clicks the add button
-$('#addTaskButton').click(function() {
-    var taskName = $('#newTaskInput').val();
+$('#addTaskButton').click(function () {
+    let taskName = $('#newTaskInput').val();
     if (taskName) {
-      var taskId = 'task' + ($('#tasksForm input[type="checkbox"]').length + 1);
-      $('#tasksForm').append('<div class="form-check"><input class="form-check-input" type="checkbox" value="' + taskName + '" id="' + taskId + '" name="tasks[]"><label class="form-check-label" for="' + taskId + '">' + taskName + '</label></div>');
-      $('#newTaskInput').val('');
+        let taskId = 'task' + ($('#tasksForm input[type="checkbox"]').length + 1);
+        $('#tasksForm').append('<div class="form-check"><input class="form-check-input" type="checkbox" value="' + taskName + '" id="' + taskId + '" name="tasks[]"><label class="form-check-label" for="' + taskId + '">' + taskName + '</label></div>');
+        $('#newTaskInput').val('');
     }
-  });
-  
-  // Save selected tasks when the user clicks the save button
-  $('#saveTasksButton').click(function() {
-    var selectedTasks = $('#tasksForm input[type="checkbox"]:checked').map(function() {
+
+});
+
+// Save selected tasks when the user clicks the save button
+$('#saveTasksButton').click(function () {
+    let selectedTasks = $('#tasksForm input[type="checkbox"]:checked').map(function () {
         return $(this).val();
-        }).get();
-        console.log(selectedTasks);
-        // do something with the selected tasks
-        // ...
-        });
+    }).get();
+    console.log(selectedTasks);
+    // do something with the selected tasks
+    // ...
+});
+
+
+$('#saveNoteButton').click(() => {
+    let podatok = {};
+
+    podatok['title'] = titleInputBox.val();
+    podatok['text'] = textNoteInputBox.val();
+
+
+    console.log(titleInputBox.val());
+    $.ajax({
+        url: 'giveNote',
+        type: 'POST',
+        dataType: 'json',
+        data: JSON.stringify(podatok),
+        success: (dataP) => {
+            console.log(dataP)
+        },
+        error: (jqXhr) => {
+            console.log(jqXhr);
+        }
+
+    })
+})
+
